960c09c24c170803482121260def5ade28d37f8b
natsuki-tacica <sns.senyou.n@gmail.com>
2019-11-06 10:41:26 +0900

    add validation for article.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :articles
+  validates :title, presence: true
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
